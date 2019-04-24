e61a8cd7dca55b12964ba2e34c0f24c899d7e0fa
AnderCat <andercat053@yahoo.com>
2019-04-25 03:38:49 +0800

    hw2 OK

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,10 @@
 function capitalize(str) {
-  return str;
+  let result = '';
+  result += str[0].toUpperCase();
+  result += str.slice(1);
+  return result;
 }
 
 console.log(capitalize('hello'));
+console.log(capitalize('Nick'));
+console.log(capitalize(',hello'));
