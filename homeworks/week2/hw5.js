530b096496fea40dfe2bc9bc683ae979d5e136fd
AnderCat <andercat053@yahoo.com>
2019-04-25 03:46:52 +0800

    HW5 OK

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
 function join(str, concatStr) {
-  return concatStr;
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (i !== 0) {
+      result += concatStr;
+    }
+    result += str[i];
+  }
+  return result;
 }
 
 function repeat(str, times) {
-  return times;
+  let result = '';
+  for (let i = 0; i < times; i += 1) {
+    result += str;
+  }
+  return result;
 }
 
-console.log(join('a', '!'));
+console.log(join([1, 2, 3], ''));
+console.log(join(['a', 'b', 'c'], '!'));
+console.log(join(['a', 1, 'b', 2, 'c', 3], ','));
+
 console.log(repeat('a', 5));
+console.log(repeat('yo', 3));
