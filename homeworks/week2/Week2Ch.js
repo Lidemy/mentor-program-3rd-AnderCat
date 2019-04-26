e20cf73726009e6f554166db5426f33fad06afe4
AnderCat <andercat053@yahoo.com>
2019-04-26 17:53:01 +0800

    Challenge fix

--- homeworks/week2/Week2Ch.js
+++ homeworks/week2/Week2Ch.js
@@ -2,13 +2,13 @@ function search(arr, n) {
   let head = 0;
   let last = arr.length - 1;
   for (let i = 0; i < arr.length; i += 1) {
-    const site = Math.round((head + last) / 2);
-    if (head === last) return -1;
+    const site = Math.floor((head + last) / 2);
     if (arr[site] === n) return site;
-    if (arr[site] > n) last = site;
-    else head = site;
+    if (head === last) return -1;
+    if (arr[site] > n) last = site - 1;
+    else head = site + 1;
   } return -1;
 }
 
-console.log(search([1, 3, 10, 14, 39], 14));
+console.log(search([1, 3, 10, 14, 39], 3));
 console.log(search([1, 3, 10, 14, 39], 299));
