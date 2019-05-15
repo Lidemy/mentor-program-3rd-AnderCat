2fef36dd4d5ad83377cf0cd19fa1a425ce336c61
AnderCat <andercat053@yahoo.com>
2019-05-15 22:07:41 +0800

    week4HwFix OK

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,25 +1,37 @@
 const request = require('request');
 const process = require('process');
 
+const website = 'https://lidemy-book-store.herokuapp.com/books';
+function listBooks() {
+  request.get(
+    website, (error, response, body) => {
+      const books = JSON.parse(body);
+      books.forEach((obj) => {
+        console.log(`${obj.id} ${obj.name}`);
+      });
+    },
+  );
+}
 
-request(
-  'https://lidemy-book-store.herokuapp.com/books',
-  (error, response, body) => {
-    const books = JSON.parse(body);
-    const bookName = books.filter(obj => obj.id === Number(process.argv[3]))
-      .map(obj => obj.name);
-    switch (process.argv[2]) {
-      case 'list':
-        books.forEach((obj) => {
-          console.log(`${obj.id} ${obj.name}`);
-        });
-        break;
-      case 'read':
-        console.log(`${process.argv[3]} ${bookName}`);
-        break;
-      default:
-        console.log('找不到此功能，請使用 list、read');
-        break;
-    }
-  },
-);
+function readBooks() {
+  request.get(
+    website, (error, response, body) => {
+      const books = JSON.parse(body);
+      const bookName = books.filter(obj => obj.id === Number(process.argv[3]))
+        .map(obj => obj.name);
+      console.log(`${process.argv[3]} ${bookName}`);
+    },
+  );
+}
+
+switch (process.argv[2]) {
+  case 'list':
+    listBooks();
+    break;
+  case 'read':
+    readBooks();
+    break;
+  default:
+    console.log('找不到此功能，請使用 list、read');
+    break;
+}
